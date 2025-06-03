23f5d705ac6906c35e016e6ded302fe7736916eb
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-03 22:16:45 +0530

    #

--- testing/pdminer_test.py
+++ testing/pdminer_test.py
@@ -1,10 +1,4 @@
 from pdfminer.high_level import extract_text
-
-# Path to the PDF file
 pdf_path = "resume.pdf"
-
-# Extract text
 text = extract_text(pdf_path)
-
-# Print the extracted text
 print(text)
